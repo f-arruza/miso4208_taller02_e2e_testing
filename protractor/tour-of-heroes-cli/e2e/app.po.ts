f61df65cd8da9a16d3ec3dc70c0ea32d1b3b22e3
f-arruza <f.arruza@uniandes.edu.co>
2018-08-22 22:17:03 -0500

    Buscar héroes

---
+++ protractor/tour-of-heroes-cli/e2e/app.po.ts
@@ -0,0 +1,29 @@
+import {browser, by, element, ElementFinder} from 'protractor';
+
+export class TourOfHeroesPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getTop4Heroes() {
+    return element.all(by.css('.module.hero')).all(by.tagName('h4')).getText();
+  }
+
+  navigateToHeroes() {
+    element(by.linkText('Heroes')).click();
+  }
+
+  getAllHeroes() {
+    return element(by.tagName('my-heroes')).all(by.tagName('li'));
+  }
+
+  enterNewHeroInInput(newHero: string) {
+    element(by.tagName('input')).sendKeys(newHero);
+    element(by.buttonText('Add')).click();
+  }
+
+  searchHeroe(hero_name: string) {
+    element(by.id('search-box')).sendKeys(hero_name);
+    return element.all(by.css('.search-result')).getText();
+  }
+}
